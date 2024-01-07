329b2c2580fd6f42c81109172b2226db451876b4
Max <maks_kosten@mail.ru>
2024-01-07 17:34:23 +0300

    function_ptr fix

--- include/Kengine/lib/lib.hxx
+++ include/Kengine/lib/lib.hxx
@@ -3,7 +3,7 @@
 namespace Kengine
 {
     typedef void* lib_handle;
-    typedef void* function_ptr;
+    typedef void (*function_ptr)(void);
 
     lib_handle   load_lib(const char* path);
     void         unload_lib(lib_handle handle);
