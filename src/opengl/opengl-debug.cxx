275a1392221409fc90a6e4d647d613f57a069d25
kostahka <maks_kosten@mail.ru>
2025-05-07 16:42:13 +0300

    Build fixes

--- src/opengl/opengl-debug.cxx
+++ src/opengl/opengl-debug.cxx
@@ -1,5 +1,7 @@
 #include "opengl-debug.hxx"
 
+#include "opengl.hxx"
+
 #undef NDEBUG
 #include "Kengine/log/log.hxx"
 
@@ -16,8 +18,9 @@ namespace Kengine::opengl_debug
         {
             glEnable(GL_DEBUG_OUTPUT);
             glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
-            glDebugMessageCallback(static_cast<GLDEBUGPROC>(&Kengine::opengl_debug::debug_message),
-                                   nullptr);
+            glDebugMessageCallback(
+                static_cast<GLDEBUGPROC>(&Kengine::opengl_debug::debug_message),
+                nullptr);
             glDebugMessageControl(
                 GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
             glDebugMessageControl(GL_DONT_CARE,
@@ -33,13 +36,13 @@ namespace Kengine::opengl_debug
         return false;
     }
 
-    void debug_message(GLenum        source,
-                                GLenum        type,
-                                GLuint        id,
-                                GLenum        severity,
-                                GLsizei       length,
-                                const GLchar* message,
-                                const void*   userParam)
+    void APIENTRY debug_message(GLenum        source,
+                       GLenum        type,
+                       GLuint        id,
+                       GLenum        severity,
+                       GLsizei       length,
+                       const GLchar* message,
+                       const void*   userParam)
     {
         std::string mess(message, length);
         KENGINE_DEBUG("{}", mess.c_str());
--- src/opengl/opengl-debug.hxx
+++ src/opengl/opengl-debug.hxx
@@ -5,12 +5,12 @@
 namespace Kengine::opengl_debug
 {
     bool initialize(int gl_major_version, int gl_minor_version, int gl_profile);
-    void debug_message(GLenum        source,
+    void APIENTRY debug_message(GLenum        source,
                                 GLenum        type,
                                 GLuint        id,
                                 GLenum        severity,
                                 GLsizei       length,
-                                const GLchar *message,
-                                const void   *userParam);
+                                const GLchar* message,
+                                const void*   userParam);
 
 } // namespace Kengine::opengl_debug
